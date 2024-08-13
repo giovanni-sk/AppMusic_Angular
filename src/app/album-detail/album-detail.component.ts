1c1308b980fc832f80100ee08b1300cad0af8eae
Gics-sk <giovanni.sokenou2105@gmail.com>
2024-08-13 07:29:01 +0100

    progress

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -9,17 +9,53 @@ import { Album, List } from '../album';
 })
 export class AlbumDetailComponent {
   @Input () selectedAlbumId ?:string;
-  currentAlbum ?:Album[];
-  SongList?:List[];
+  currentAlbum ?:Album;
+  SongList?:List;
+
+
+  isPlaying: boolean = false;
+  progress: number = 0;
+  currentSongIndex: number = 0;
+  totalSongs: number = 0;
+  intervalId?: number;
   constructor(private myService: AlbumService){}
   ngOnChanges(){
     if (this.selectedAlbumId) {
-      this.currentAlbum=this.myService.getAllAlbum().filter(param=>{
+      this.currentAlbum=this.myService.getAllAlbum().find(param=>{
         return param.id==this.selectedAlbumId;
       })
-      this.SongList=this.myService.getListSong().filter(param=>{
+      this.SongList=this.myService.getListSong().find(param=>{
         return param.id == this.selectedAlbumId;
       })
+    }
+  }
+  playAlbum(): void {
+    if (this.currentAlbum) {
+      this.isPlaying = true;
+      this.progress = 0;
+      this.currentSongIndex = 0;
+
+      const totalDuration = this.currentAlbum.duration; // total duration in seconds
+      const intervalDuration = totalDuration / 100; // 1% of the total duration
+
+      this.intervalId = window.setInterval(() => {
+        this.progress += 1;
+
+        if (this.progress >= 100) {
+          this.stopAlbum();
+        } else if (this.progress % (100 / this.totalSongs) === 0) {
+          this.currentSongIndex++;
+        }
+      }, intervalDuration * 1000); // converting interval duration to milliseconds
+    }
+  }
+
+  stopAlbum(): void {
+    this.isPlaying = false;
+    this.progress = 0;
+    this.currentSongIndex = 0;
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
     }
   }
 
